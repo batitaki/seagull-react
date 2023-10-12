d4d5d5f129e9420e90a14614674a3c740abccdf1
User <taquinibautista@gmail.com>
2023-10-11 21:55:38 -0300

    m

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import EscuelasList from './components/escuelasList'; // Asegúrate de que la ruta sea correcta
 import ProductMetrics from './components/productMetrics'; // Importa el componente ProductMetrics
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* Otras partes de tu aplicación */}
-        <EscuelasList /> {/* Aquí incluyes el componente EscuelasList */}
         <ProductMetrics /> {/* Aquí incluyes el componente ProductMetrics */}
       </header>
     </div>
